9f4371ad0a70d865ad089fd0c7837628ec889d6b
Denys <pronindenis91@gmail.com>
2024-08-29 18:51:31 +0100

    inital commit

---
+++ weather-app/weather-app/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        Have a nice day and don't forget umbrella if you are in New Delhi now!
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ weather-app/weather-app/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      Weather Forecast <i className="wi wi-sunrise"></i>
+    </header>
+  );
+}
+
+export default Header;
